9b7b4a87e4aff6c54a34048f3afb999c74a71588
Julien Jean-Pierre <julienjeanpierre97@gmail.com>
2018-02-16 20:09:17 -0600

    Add Loop to continously check

--- lib/get_tri_options.py
+++ lib/get_tri_options.py
@@ -2,7 +2,6 @@
 # For Binance REST API:		https://github.com/binance-exchange/binance-official-api-docs
 # Download time ~ 1.5s
 # Runtime ~ .005seconds
-
 import time
 import requests
 import pprint
@@ -10,6 +9,7 @@ import pickle
 import numpy as np
 import sys
 pp = pprint.PrettyPrinter()
+
 
 class TryBot:
 	def __init__(self):
@@ -20,14 +20,28 @@ class TryBot:
 		self.ETH_Opps = []
 		self.BNB_Opps = []
 		self.USDT_Opps = []
-		self.self.all_percentages = []
-		self.pairs = {}
-		self.all_orders = {}
+		self.all_percentages = []
+		self.transaction_fee = .001
+	
+		self.pairs = (requests.get('https://api.binance.com/api/v3/ticker/price')).json()
+		self.all_orders = (requests.get('https://api.binance.com/api/v3/ticker/bookTicker')).json()
+
+
+	def resetVals(self):
+		self.BTC_Pairs = []
+		self.ETH_Pairs = []
+		self.BNB_Pairs = []
+		self.USDT_Pairs = []
+		self.ETH_Opps = []
+		self.BNB_Opps = []
+		self.USDT_Opps = []
+		self.all_percentages = []
+		# self.pairs = {}
+
 
 	# Get all the currency pairs and there prices on Binance
-	def updatePairs(self):
-		target = '/api/v3/ticker/price'
-		self.pairs = (requests.get('https://api.binance.com/' + target)).json()
+	def updateOrders(self):
+		self.all_orders = (requests.get('https://api.binance.com/api/v3/ticker/bookTicker')).json()
 		
 
 	# Make json interactable
@@ -39,7 +53,7 @@ class TryBot:
 
 	# Create List of pairs for above lists ^
 	def createCurrencyPairs(self):
-		for pair in pairs:
+		for pair in self.pairs:
 			curr_pair = pair['symbol']
 
 			if 'BTC' == curr_pair[-3:]:
@@ -102,28 +116,28 @@ class TryBot:
 				return i['bidPrice']
 
 	# self.all_percentages = []
-	transaction_fee = .001
+	
 	def createTripletData(self):
 		for self.ETH_Path in self.ETH_Opps:
 			curr_split = self.ETH_Path.split('_')
 			principle = 1.0
 			
-			step1 = principle / float(getAsk(curr_split[1] + curr_split[0])) * (1.0 - transaction_fee)
-			step2 = step1/float(getAsk(curr_split[2] + curr_split[1])) * (1.0 - transaction_fee)
-			newPrice = step2 * float(getBid(curr_split[2] + curr_split[0])) * (1.0 - transaction_fee)
+			step1 = principle / float(self.getAsk(curr_split[1] + curr_split[0])) * (1.0 - self.transaction_fee)
+			step2 = step1/float(self.getAsk(curr_split[2] + curr_split[1])) * (1.0 - self.transaction_fee)
+			newPrice = step2 * float(self.getBid(curr_split[2] + curr_split[0])) * (1.0 - self.transaction_fee)
 			if newPrice > 1.0:
-				self.all_percentages.append([self.ETH_Path, (100*((newPrice/1.0) - 1)),getAsk(curr_split[1] + curr_split[0]) ,getAsk(curr_split[2] + curr_split[1]), getBid(curr_split[2] + curr_split[0])])
+				self.all_percentages.append([self.ETH_Path, (100*((newPrice/1.0) - 1)),self.getAsk(curr_split[1] + curr_split[0]) ,self.getAsk(curr_split[2] + curr_split[1]), self.getBid(curr_split[2] + curr_split[0])])
 			# print('Net Gain: {}'.format((100*((newPrice/1.0) - 1))))
 
 		for BNB_Path in self.BNB_Opps:
 			curr_split = BNB_Path.split('_')
 			principle = 1.0
 			
-			step1 = principle / float(getAsk(curr_split[1] + curr_split[0])) * (1.0 - transaction_fee)
-			step2 = step1/float(getAsk(curr_split[2] + curr_split[1])) * (1.0 - transaction_fee)
-			newPrice = step2 * float(getBid(curr_split[2] + curr_split[0])) * (1.0 - transaction_fee)
+			step1 = principle / float(self.getAsk(curr_split[1] + curr_split[0])) * (1.0 - self.transaction_fee)
+			step2 = step1/float(self.getAsk(curr_split[2] + curr_split[1])) * (1.0 - self.transaction_fee)
+			newPrice = step2 * float(self.getBid(curr_split[2] + curr_split[0])) * (1.0 - self.transaction_fee)
 			if newPrice > 1.0:
-				self.all_percentages.append([BNB_Path, (100*((newPrice/1.0) - 1)),getAsk(curr_split[1] + curr_split[0]) ,getAsk(curr_split[2] + curr_split[1]), getBid(curr_split[2] + curr_split[0])])
+				self.all_percentages.append([BNB_Path, (100*((newPrice/1.0) - 1)),self.getAsk(curr_split[1] + curr_split[0]) ,self.getAsk(curr_split[2] + curr_split[1]), self.getBid(curr_split[2] + curr_split[0])])
 			# print('Net Gain: {}'.format((100*((newPrice/1.0) - 1))))
 
 		for USDT_Path in self.USDT_Opps:
@@ -131,43 +145,46 @@ class TryBot:
 			principle = 1.0
 			
 
-			step1 = principle * float(getAsk(curr_split[0] + curr_split[1])) * (1.0 - transaction_fee)
+			step1 = principle * float(self.getAsk(curr_split[0] + curr_split[1])) * (1.0 - self.transaction_fee)
 			# print('1 {} buys {} {}'.format(curr_split[0], step1, curr_split[1]))
 
-			step2 = step1/float(getAsk(curr_split[2] + curr_split[1])) * (1.0 - transaction_fee)
+			step2 = step1/float(self.getAsk(curr_split[2] + curr_split[1])) * (1.0 - self.transaction_fee)
 			# print('{} {} buys {} {}'.format(step1,curr_split[1], step2, curr_split[2]))
 
-			newPrice = step2 * float(getBid(curr_split[2] + curr_split[0])) * (1.0 - transaction_fee)
+			newPrice = step2 * float(self.getBid(curr_split[2] + curr_split[0])) * (1.0 - self.transaction_fee)
 			# print('{} {} buys {} {}'.format(step2, curr_split[2], newPrice, curr_split[0]))
 
 			if newPrice > 1.0:
-				self.all_percentages.append([USDT_Path, (100*((newPrice/1.0) - 1)), getAsk(curr_split[0] + curr_split[1]) ,getAsk(curr_split[2] + curr_split[1]), getBid(curr_split[2] + curr_split[0])])
+				self.all_percentages.append([USDT_Path, (100*((newPrice/1.0) - 1)), self.getAsk(curr_split[0] + curr_split[1]) ,self.getAsk(curr_split[2] + curr_split[1]), self.getBid(curr_split[2] + curr_split[0])])
 			# print('Net Gain: {}'.format((100*((newPrice/1.0) - 1)))))
 			# print('Net Gain: {}'.format((100*((newPrice/1.0) - 1))))
 			# print(getPrice(all_opps[-1][0].split('_')[2] + all_opps[-1][0].split('_')[1]))
 
 
 def main():
-
+	timeTotal = time.time()
 	# Run this continoously
+	bot = TryBot()
 	while True:
-
+		bot.resetVals()
 		t0 = time.time()
-		self.all_orders = getAllOrders()
+		# bot.all_orders = getAllOrders()
+		# bot.updatePairs()
 
-		createCurrencyPairs()
-		createCurrencyOpps()
-		createTripletData()
 
-		# print(self.all_percentages)
-		if len(self.all_percentages) > 0:
-			all_opps = np.array(self.all_percentages)
+		bot.createCurrencyPairs()
+		bot.createCurrencyOpps()
+		bot.createTripletData()
+		if len(bot.all_percentages) > 0:
+			all_opps = np.array(bot.all_percentages)
 			all_opps=all_opps[np.argsort(all_opps[:,1])]
-			print(all_opps)
+			print(all_opps[:,:2])
 		else:
 			print('No Triplets Found.')
-		t1 = time.time()
-		print('Runtime {} seconds'.format(t1-t0))
+		bot.updateOrders()
+		print('Runtime {} seconds'.format(time.time()-t0))
+
+	print("Total time took {} seconds".format(time.time() - timeTotal))
 
 if __name__ == '__main__':
 	main()
